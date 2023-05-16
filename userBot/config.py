f8eb0a2b995db0162b6ecf1f937e46f173d78410
Ömer Şayak <103234028+omersayak@users.noreply.github.com>
2023-05-16 22:16:32 +0300

    Update config.py

--- userBot/config.py
+++ userBot/config.py
@@ -1,6 +1,6 @@
-api_id = input("Telegram API ID'nizi girin: ")
-api_hash = input("Telegram API hash'inizi girin: ")
-admin_id = input("Admin kullanıcının ID'sini girin: ")
-api_stack = input("Api Stack Sayasından alınan api: ")
+api_id = int(input("Telegram API ID'nizi girin: "))
+api_hash = str(input("Telegram API hash'inizi girin: "))
+admin_id = int(input("Admin kullanıcının ID'sini girin: "))
+api_stack = str(input("Api Stack Sayasından alınan api: "))
 
 
